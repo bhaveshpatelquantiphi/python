24162a17b4f0b18c20631fe10fa656580d600718
Bhavesh Patel <bhavesh.patel@quantiphi.com>
2017-07-17 12:03:56 +0530

    Assessment code added

--- Assessment/q14.py
+++ Assessment/q14.py
@@ -1,4 +1,16 @@
 # 14.Write a Python program to check if a given positive integer is a power of two
 
-x = raw_input("Enter a positive number")
+x = int(raw_input("Enter a positive number"))
+flag = False
+while (True):
+	if (x%2 == 0 and x!=1):
+		flag = True
+		x/=2
+		print x
+	else:
+		print "not a power of two"
+		flag = False
+		break;
 
+if(flag):
+	print "Power of two"
